364f519a30947990b7691c94c42b9200c4a5dc4f
fumi23 <78896035+fumi1623@users.noreply.github.com>
2021-07-21 04:33:16 +0900

    [finish]ep6

--- ep1/src/ep5/UseTwoRobot.java
+++ ep1/src/ep5/UseTwoRobot.java
@@ -1,14 +1,14 @@
-package ep5;
-
-public class UseTwoRobot {
-	public static void main(String[] args) {
-		Robot a = new Robot();
-		a.name = "A";
-		
-		Robot b = new Robot();
-		b.name = "B";
-		
-		a.sayHello();
-		b.sayHello();
-	}
-}
+//package ep5;
+//
+//public class UseTwoRobot {
+//	public static void main(String[] args) {
+//		Robot a = new Robot();
+//		a.name = "A";
+//		
+//		Robot b = new Robot();
+//		b.name = "B";
+//		
+//		a.sayHello();
+//		b.sayHello();
+//	}
+//}
