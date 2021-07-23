1c44b80ff9bcad1ba8b12131c5a766c27827c014
fumi23 <78896035+fumi1623@users.noreply.github.com>
2021-07-24 07:26:35 +0900

    [finish]ep9

---
+++ ep1/src/ep9/A.java
@@ -0,0 +1,11 @@
+package ep9;
+
+public class A {
+//	private int num;
+//	public void setNum(int num) {
+//		this.num = num;
+//	}
+	public void hello() {
+		System.out.println("hello.");
+	}
+}
---
+++ ep1/src/ep9/B.java
@@ -0,0 +1,12 @@
+package ep9;
+
+public class B extends A {
+//	private int num;
+//	public int getNum() {
+//		return num;
+//	}
+	public void hello() {
+		super.hello();
+		System.out.println("ハロー");
+	}
+}
